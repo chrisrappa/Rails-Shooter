9097312ba40ff3a74406e15820e7b7b55a14e5d9
rappaproductions <rappaproductionsinc@gmail.com>
2020-05-06 19:06:23 -0500

    Ship flies and shoots without input; Terrain mocked up and styled; Enemies placed but not coded.

---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerShip : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float speed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 15f;
+
+    [Tooltip("In m")] [SerializeField] float yRange = 8f;
+    // Start is called before the first frame update
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -40;
+
+    float xThrow, yThrow;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        changeVerticalPosition();
+        changeHorizontalPosition();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transformPitch();
+        float yaw = transformYaw();
+        float roll = transformRoll();
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private float transformRoll()
+    {
+        float rollDuetoControlThrow = xThrow * controlRollFactor;
+        float roll = rollDuetoControlThrow;
+        return roll;
+    }
+
+    private float transformYaw()
+    {
+        float yawDuetoPosition = transform.localPosition.x * positionYawFactor;
+        float yawDuetoControlThrow = xThrow * controlYawFactor;
+        float yaw = yawDuetoPosition + yawDuetoControlThrow;
+        return yaw;
+    }
+
+    private float transformPitch()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        return pitch;
+    }
+
+    private void changeHorizontalPosition()
+    {
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime * 5f;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+
+    private void changeVerticalPosition()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime * 5f;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
---
+++ Assets/Scripts/SceneSwap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        int xCenter = (Screen.width / 2);
+        int yCenter = (Screen.height / 2);
+        int width = 400;
+        int height = 120;
+
+        GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("button"));
+        fontSize.fontSize = 32;
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "Start Screen")
+        {
+            // Show a button to allow scene2 to be switched to.
+            if (GUI.Button(new Rect(xCenter - width / 2, yCenter - height / 2, width, height), "Load second scene", fontSize))
+            {
+                SceneManager.LoadScene("Level 1");
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
