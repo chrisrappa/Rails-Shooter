2667396cb21d941750fa1ce58acf30fd89a64ca9
rappaproductions <rappaproductionsinc@gmail.com>
2020-06-01 22:16:48 -0500

    Enemies change score when hit; Scoreboard Added

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -7,20 +7,18 @@ using UnityStandardAssets.Effects;
 public class CollisionHandler : MonoBehaviour
 {
     [Tooltip("In seconds")] [SerializeField] float levelLoadDelay = 1f;
-    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
+    [Tooltip("FX prefab on player")] [SerializeField] public GameObject deathFX;
 
     private void OnTriggerEnter(Collider other)
     {
         startDeathSequence();
         deathFX.SetActive(true);
+        Invoke("LoadNextScene", levelLoadDelay);
     }
 
     private void startDeathSequence()
     {
-
-        Invoke("LoadNextScene", levelLoadDelay);
         gameObject.SendMessage("playerDied");
-
     }
     private void LoadNextScene()
     {
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UIElements;
+
+public class Enemy : MonoBehaviour
+{
+    [Tooltip("FX prefab on enemy")] [SerializeField] public GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        
+    }
+
+    public void AddBoxCollider ()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+
+    }
+
+
+
+
+}
